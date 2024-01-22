623d3c6c958d49f4d05d64b9537d144f50756d20
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-01-22 21:31:59 +0100

    style: fix adaptive design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,34 +1,29 @@
 export default function Footer() {
   return (
-    <footer
-      className={'flex h-24 w-full items-center justify-start bg-black text-inverted sm:flex-row'}>
-      <div
-        className={' ml-6 text-lg sm:mx-36 sm:flex sm:items-center sm:gap-36'}
-      >
-        <ul
-          className={
-            'flex flex-wrap gap-x-3 sm:order-2 sm:flex-col sm:justify-center'
-          }
-        >
-          <li>
+    <footer className={'h-24 w-full bg-black text-inverted   '}>
+      <div className={'sm:flex items-center  mx-6 sm:mx-20 sm:gap-40 sm:w-[1144px]'}>
+        <div className={'mt-3'}>
+          <div>
             <a href="tel:(555) 123-45675" rel="noopener noreferrer">
               Phone Number: (555) 123-4567
             </a>
-          </li>
-          <li>
+          </div>
+          <div>
             Email:
             <span className={'ml-1 underline  sm:no-underline'}>
               <a
                 href="mailto:info@coffeetimecafe.com"
-                rel="noopener noreferrer"
-              >
+                rel="noopener noreferrer">
                 info@coffeetimecafe.com
               </a>
             </span>
-          </li>
-        </ul>
-        <p className={'sm:order-1 sm:mb-0'}>© 2023 Iced Latte</p>
+          </div>
+        </div>
+        <div>
+          <p className={'mt-2 sm:mt-0 sm:mb-0'}>© 2024 Iced Latte</p>
+        </div>
       </div>
-    </footer>
+    </footer >
   )
 }
+
